62f3b67cff309606cf9fb077fedd8f17c10148c4
Thyago Barbosa Rodrigues <thyagobr@gmail.com>
2014-11-27 08:29:24 -0300

    Sets up first link and simple transitions

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -1,4 +1,11 @@
-angular.module("solde", [])
+angular.module("solde", ['ui.router'])
+  .config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('quemsomos', {
+        url: '/quemsomos',
+      templateUrl: "partials/about.html"
+      });
+  })
   .controller('test', ["$scope", function ($scope) {
   $scope.list = ['thyago', 'rocks', 'dadocks'];
 }]);
