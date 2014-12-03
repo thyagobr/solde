bcdd4418138721b5afc4c575eb3a945303a4d902
Thyago Barbosa <thyagobr@gmail.com>
2014-12-03 10:18:05 -0300

    Functional after server-side CORS authorization

--- assets/javascripts/payment.js
+++ assets/javascripts/payment.js
@@ -1,10 +1,14 @@
 var app = angular.module("payment", []);
 
-app.controller('listController', ['$scope', function($scope) {
+app.controller('listController', ['$scope', '$http', function($scope, $http) {
   $scope.list = [];
 
   $scope.cadastrar = function() {
-    $scope.list.push($scope.pay);
+    $scope.list.push({fornecedor: $scope.pay.fornecedor, value: $scope.pay.value, due_date: $scope.pay.vencimento});
     console.log($scope.list);
+    $http.get("http://0.0.0.0:3000/obligations/1.json")
+  .success(function(data) {
+    console.log(data);
+  });
   };
 }]);
