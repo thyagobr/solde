63c2cb3e6491e886ff12171d7a735c60aa7a3aac
Thyago Barbosa Rodrigues <thyagobr@gmail.com>
2014-12-05 11:11:36 -0300

    Fields set on view.

--- assets/javascripts/payment.js
+++ assets/javascripts/payment.js
@@ -1,9 +1,19 @@
 var app = angular.module("payment", []);
 
-app.controller('listController', ['$scope', '$http', function($scope, $http) {
+app.controller('paymentsController', ['$scope', '$http','$filter', function($scope, $http, $filter) {
+  // Creates payment object
+  $scope.pay = new Object();
+  $scope.pay.date = $filter('date')(new Date(), 'dd/MM/yyyy');
+
+  // Generates numer_of_payments select
+  $scope.pay.list_number_of_payments = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+  $scope.pay.number_of_payments = 1;
+
+  // Fetches current obligations on server
   $http.get("http://0.0.0.0:3000/obligations.json").success(function(data) {
     $scope.list = data;
   });
+
 
   $scope.cadastrar = function() {
     var obligation = {company: $scope.pay.fornecedor, value: $scope.pay.valor, due_date: $scope.pay.vencimento};
